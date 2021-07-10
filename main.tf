f6e99310bfa277109170a574cdf78ef5f97705b4
Miquido Ops <mqtc@miquido.com>
2021-07-10 05:06:26 +0000

    chore(deps): bump ecs-alb-task-grafana from 5.6.5 to 5.6.7
    Bumps [ecs-alb-task-grafana](https://gitlab.com/miquido/terraform) from 5.6.5 to 5.6.7.
    - [Release notes](https://gitlab.com/miquido/terraform/tags)
    - [Commits](https://gitlab.com/miquido/terraform/compare/tags/5.6.5...tags/5.6.7)

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_route53_record" "grafana" {
 }
 
 module "ecs-alb-task-grafana" {
-  source                            = "git::ssh://git@gitlab.com/miquido/terraform/terraform-ecs-alb-task.git?ref=tags/5.6.5"
+  source                            = "git::ssh://git@gitlab.com/miquido/terraform/terraform-ecs-alb-task.git?ref=tags/5.6.7"
   name                              = var.service_name
   project                           = var.project
   environment                       = var.environment
